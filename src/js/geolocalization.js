b1be178e7c50d70794cc30cc5e561bb6f5a2fdce
CarlsKe <fernancarldiaz@outlook.com>
2017-08-14 22:03:57 -0500

    feat: added geolocalization for the map and places

---
+++ src/js/geolocalization.js
@@ -0,0 +1,23 @@
+(function geolocalization(){
+    'use strict';
+
+    class GeolocalizationCords{
+        getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.watchPosition(this.showPosition);
+            } else { 
+                console.log("Geolocation is not supported by this browser.")
+                }
+        }
+
+        showPosition(position) {
+            console.log("Latitude: " + position.coords.latitude);
+            console.log("Longitude: " + position.coords.longitude)
+        }
+
+    }
+
+    let geo = new GeolocalizationCords();
+    window.onload = geo.getLocation();
+
+})();
