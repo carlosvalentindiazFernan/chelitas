dcda0dc97e10c16e52f13cd806d65cfe6a98f72d
aldo <aldo@karens-MacBook-Pro.local>
2017-08-16 08:04:43 -0500

    Last commit

--- src/map-chelitas/map.js
+++ src/map-chelitas/map.js
@@ -1,27 +1,25 @@
 (function Map(){
       'use strict';
 
-
-
       class MapChelitas extends Polymer.Element {
 
-          static get is() { return 'map-chelitas'; }
+        static get is() { return 'map-chelitas'; }
 
-          static properties(){
+        static properties(){
             return {
 
-              latitude:{
+                latitude:{
                 type: String,
                 computed: "_changelatitude"
-              },
+                },
 
-              longitude: {
+                longitude: {
                 type: String,
                 computed: "_changelongitude"
-              }
+                }
 
             };
-          }
+        }
 
           ready(){
             super.ready();
@@ -35,14 +33,16 @@
           if (navigator.geolocation) {
             navigator.geolocation.watchPosition(this.showPosition);
           } else {
-            console.log("Geolocation is not supported by this browser.")
+            console.log("Geolocation is not supported by this browser.");
           }
         }
 
         showPosition(position) {
-          console.log(position)
+          console.log(position);
           this.latitude = position.coords.latitude;
+          console.log(this.latitude);
           this.longitude = position.coords.longitude;
+          console.log(this.longitude);
         }
 
 
